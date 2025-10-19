93fc00c909c86eb68a8675a274b6788c0f413ae0
unknown <carlosotoline@gmail.com>
2025-10-18 23:27:39 -0300

    login page

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,10 +1,11 @@
 import {Routes, Route} from "react-router-dom";
 import { CadastroPage } from "../pages/CadastroPage/cadastro";
+import { LoginPage } from "../pages/LoginPage/login"
 
 export function Rotas(){
     return(
         <Routes>
-            {/* <Route path="/login" element={<LoginPage/>} /> */}
+            <Route path="/" element={<LoginPage/>} />
             <Route path="/cadastro" element={<CadastroPage/>} />
         </Routes>
     )
--- src/pages/LoginPage/login.jsx
+++ src/pages/LoginPage/login.jsx
@@ -12,14 +12,36 @@ export function LoginPage() {
     }
     
     return(
-        <div className={styles.backgroudMain}>
-            <h2>Login</h2>
+        <form onSubmit={handleSubmit}>
 
-            <div>
-                
+            <div className={styles.backgroudMain}>
+
+                <h2>Pagina de Login</h2>
+
+                <div className={styles.formBoxLogin}>
+                    <h2>Email</h2>
+                    <input
+                        type="text"
+                        placeholder="Insira seu email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)} />
+                </div>
+
+                <div className={styles.formBoxLogin}>
+                    <h2>Senha</h2>
+                    <input 
+                        type="text"
+                        placeholder="Insira sua senha"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)} />
+                </div>
+
+                <div className={styles.botao}>
+                    <button type="submit">Entrar</button>
+                </div>
+
             </div>
-
-        </div>
+        </form>
         
     )
 }
